9e4f4f66d40759ebcc67e01ba4ad4e0869f95a0b
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-09-01 13:40:12 +0700

    Modified the get method of the views to check for invalid urls

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+# from django.contrib.messages.views import
 from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
@@ -25,13 +26,41 @@ class DetailView(generic.DetailView):
         """Only return questions that are currently published"""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+    def get(self, request, *args, **kwargs):
+        """
+        Override the get() method and checks if the poll is valid to access
+        """
+        try:
+            Question.objects.get(pk=kwargs['pk'])
+        except Question.DoesNotExist:
+            return HttpResponseRedirect(reverse("polls:index"))
+        question = Question.objects.get(pk=kwargs['pk'])
+        if not question.is_published():
+            return HttpResponseRedirect(reverse("polls:index"))
+        return super().get(request, *args, **kwargs)
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+    def get(self, request, *args, **kwargs):
+        """
+        Override the get() method checks if
+        the poll's result is valid to access
+        """
+        try:
+            Question.objects.get(pk=kwargs['pk'])
+        except Question.DoesNotExist:
+            return HttpResponseRedirect(reverse("polls:index"))
+        question = Question.objects.get(pk=kwargs['pk'])
+        if not question.is_published():
+            return HttpResponseRedirect(reverse("polls:index"))
+        return super().get(request, *args, **kwargs)
+
 
 def vote(request, question_id):
+    """Handler for submitting a vote"""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -45,4 +74,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question_id,)))
