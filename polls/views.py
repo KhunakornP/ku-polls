1d18df593f92f01564d8ea6451f242bccd81d284
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-09-07 14:58:27 +0700

    Created a user registration page

--- polls/views.py
+++ polls/views.py
@@ -2,11 +2,13 @@ import logging
 
 from django.contrib.auth import (user_logged_in, user_logged_out,
                                  user_login_failed)
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.dispatch import receiver
 from django.urls import reverse
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
@@ -123,6 +125,22 @@ def vote(request, question_id):
                     f"question {question_id}.) {question.question_text}")
         return HttpResponseRedirect(
             reverse("polls:results", args=(question_id,)))
+
+
+def register(request):
+    """Handler for creating new users"""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_passwd = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_passwd)
+            login(request, user)
+        return redirect('polls:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 
 def get_client_ip(request):
